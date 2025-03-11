6bd11236657ec75411a983c316a87a9d12a500a6
Antoine Crocherie <antoine.crocherie@gmail.com>
2025-03-11 12:51:47 +0100

    all controller except geoscoring and welcome

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,24 @@
 class RequestsController < ApplicationController
-def index
-end
-def show
-end
-def update
-end
+  def index
+    @requests = Request.all
+  end
+
+  def show
+    @request = Request.find(params[:id])
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    if @request.update(request_params)
+      redirect_to @request
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:attribute1, :attribute2)
+  end
 end
