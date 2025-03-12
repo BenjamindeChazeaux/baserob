179233e979147c9b9caeaba4bd0896bc0a051646
Benjamin de Chazeaux <dechazeauxb@gmail.com>
2025-03-12 11:08:21 +0100

    AI analytics

--- config/routes.rb
+++ config/routes.rb
@@ -81,4 +81,7 @@ Rails.application.routes.draw do
   get 'profile', to: 'users#show'
 
   get 'requests', to: 'requests#index', as: 'requests_index'
+
+  # Route pour les analytics IA
+  get 'ai_analytics', to: 'analytics#ai', as: :ai_analytics
 end
