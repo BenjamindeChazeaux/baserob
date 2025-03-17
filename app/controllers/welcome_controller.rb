091d7473fe5b21f9a8a285e78214f9a6ace6cc16
anh nguyen <nguyenvanhoanganh7994@gmail.com>
2025-03-17 21:00:35 +0100

    update pie-chart in page welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
+
   def index
     @company = Company.new
+
+    real_company = Company.first
+    @total_requests_count = real_company.requests.count
+    @ai_requests_count = real_company.requests.where.not(ai_provider_id: nil).count
+    # @non_ai_requests_count = real_company.requests.count - @ai_requests_count
+
   end
 
   def show
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import "flip_cards"
-
 import "Chart.bundle"
 import "chartkick"
 // import Highcharts from 'highcharts';
