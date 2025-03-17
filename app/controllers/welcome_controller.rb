c87c711fa473c1010769a30d11f7d7f23c092ed2
anh nguyen <nguyenvanhoanganh7994@gmail.com>
2025-03-17 21:16:03 +0100

    update pie-chart in page welcome v2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,16 @@ class WelcomeController < ApplicationController
   def index
     @company = Company.new
 
-    real_company = Company.first
-    @total_requests_count = real_company.requests.count
-    @ai_requests_count = real_company.requests.where.not(ai_provider_id: nil).count
+
+    if current_user&.company.present?
+      real_company = current_user.company
+      @total_requests_count = real_company.requests.count
+      @ai_requests_count = real_company.requests.where.not(ai_provider_id: nil).count
+    else
+      real_company = nil
+      @total_requests_count = 0
+      @ai_requests_count = 0
+    end
     # @non_ai_requests_count = real_company.requests.count - @ai_requests_count
 
   end
